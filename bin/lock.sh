93db4d4f75e40d2345e60d891cc83d0ee5ec65de
Rohan Rao <rao.rohan.a@gmail.com>
2019-09-15 02:47:38 -0400

    Sun 15 Sep 2019 02:47:38 AM EDT

--- bin/lock.sh
+++ bin/lock.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 
-i3lock -u -i <(import -screen -window root -silent png:- | convert png:- -scale 5% -scale 2000% png:- | convert png:- $HOME/pics/lock.png -gravity center -composite -matte png:-)
+import -screen -window root -silent png:- | convert png:- -scale 5% -scale 2000% -depth 8 png:- | convert png:- $HOME/pics/lock.png -gravity center -composite -matte RGB:- | i3lock -u --raw 1920x1080:rgb -i /dev/stdin
